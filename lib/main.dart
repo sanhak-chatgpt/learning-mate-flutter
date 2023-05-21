ea6fbd6f227d27e09fc75669f10cc4998ff1b79d
kjyook <106497893+kjyook@users.noreply.github.com>
2023-05-21 14:51:28 +0900

    feat: package추가, store호출 함수, mic권한 요청 함수

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:learningmate/screens/webview_screen.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:in_app_review/in_app_review.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +13,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String channelName = 'your_channel_name';
+  static const String channelName = 'the_learningmate';
 
   final MethodChannel _channel = const MethodChannel(channelName);
 
@@ -51,20 +53,59 @@ class _MyWebViewState extends State<MyWebView> {
         onWebViewCreated: (WebViewController webViewController) {
           _controller.complete(webViewController);
         },
-        javascriptChannels: <JavascriptChannel>{
+        javascriptChannels: <JavascriptChannel>[
           JavascriptChannel(
-            name: 'Microphone',
+            name: 'FlutterBridge',
             onMessageReceived: (JavascriptMessage message) {
-              requestMicrophone();
+              switch(message){
+                case 'requestMicrophonePermission':
+                  requestMicrophone();
+                  break;
+                case 'requestOpenStoreListing':
+                  openStoreListing();
+                  break;
+                default:
+                  break;
+              }
               widget.channel.invokeMethod('your_method_name', message.message);
             },
           ),
-        },
+        ].toSet();,
       ),
     );
   }
 
-  void requestMicrophone() async {
-    // 마이크 권한 요청
+  Future<void> requestMicrophone() async {
+    // 마이크 권한 요청 코드 작성
+    PermissionStatus status = await Permission.microphone.request();
+
+    //권한 요청 결과에 따라 front에 message를 보냄
+    final webViewController = await _controller.future;
+    if(status){
+      webViewController.evaluateJavascript('MicrophonePermissionBridge.receiveMessage(${GRANTED})');
+    }else{
+      webViewController.evaluateJavascript('MicrophonePermissionBridge.receiveMessage(${DENIED})');
+    }
+  }
+
+  Future<void> openStoreListing(BuildContext context) async {
+    // Flutter 앱에서 openStoreListing을 호출하는 로직을 작성하세요
+    final InAppReview inAppReview = InAppReview.instance;
+    final webViewController = await _controller.future;
+
+    if (await inAppReview.isAvailable()) {
+      final bool isOpened = await inAppReview.openStoreListing(appStoreId: '1662203668');
+      if (isOpened) {
+        // 앱 스토어 리뷰 페이지가 성공적으로 열린 경우 처리할 로직 작성
+        webViewController.evaluateJavascript('OpenStoreListBridge.receiveMessage(${GRANTED})');
+      } else {
+        // 앱 스토어 리뷰 페이지를 열지 못한 경우 처리할 로직 작성
+        webViewController.evaluateJavascript('OpenStoreListBridge.receiveMessage(${DENIED})');
+      }
+    } else {
+    // In-App Review가 사용 불가능한 경우 처리할 로직 작성
+      webViewController.evaluateJavascript('OpenStoreListBridge.receiveMessage(${DENIED})');
+    }
+    //InAppReview.instance.openStoreListing(appStoreId: '1662203668');
   }
 }
